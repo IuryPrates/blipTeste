b7c1cb45473a5b1c084f3b5a163b4ae655ab708e
IuryPrates <prates700@gmail.com>
2024-11-06 00:03:01 -0400

    create routes.js

---
+++ testeBlip/src/routes.js
@@ -0,0 +1,29 @@
+const routes = require("express").Router();
+const githubApi = require("./apiGithub/githubApi");
+
+routes.get("/repos", async (req, res) => {
+  try {
+    
+    const { data } = await githubApi.get("/users/takenet/repos?&sort=created&direction=asc");
+    const filtredValue = data.filter(repo => repo.language === 'C#').slice(0, 5);
+    const buildJsonOutput = filtredValue.map(repo => ({
+      title: repo.full_name,
+      text: repo.description,
+      type: "image/jpeg",
+      uri: repo.owner.avatar_url
+    }))
+
+    // Adiciona id para cada registro objeto do repositorio
+    const result = {};
+    buildJsonOutput.map((repo, index) => {
+      result[index] = repo;
+    })
+ 
+    res.status(200).json(result).end();
+
+  } catch (error) {
+    res.send({ error: error.message })
+  }
+})
+
+module.exports = routes;
