d9d618ce393aeead05a5fb83738c873bd623860e
IuryPrates <prates700@gmail.com>
2024-11-05 23:59:43 -0400

    create index.js

---
+++ testeBlip/src/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(require('./routes'));
+
+app.listen(port, function () {
+  console.log("App Listen on port >> " + port);
+});
